2c31104a5009a0f592fb9137cdf314b77f173ee7
arunvemana <arunsai4b6@gmail.com>
2025-04-18 22:05:52 +0530

    working home screen line

--- UI/board.py
+++ UI/board.py
@@ -1,7 +1,24 @@
 import os.path
-from idlelib.browser import transform_children
 
 import flet as ft
+
+
+home_run:list= [(6,1),(7,1),(7,3),(7,4),(7,5),(7,6),(1,8),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),(8,15),(7,13),(7,12),(7,11),(7,10),(7,9),(7,8)]
+# def draw_lines_to_center(page):
+#     center_x, center_y = 300,300
+#     player_positions = {
+#         "player1" : (100,100),
+#         "player2" : (500,100),
+#         "player3" : (100,500),
+#         "player4" : (500,500)
+#     }
+#
+#     shapes = []
+#     for player, (x, y) in player_positions.items():
+#         shapes.append(ft.PaintLinearGradient(start=(x, y), end=(center_x, center_y), color=ft.Colors.BLUE, stroke_width=2))
+#
+#     page.add(*shapes)
+
 
 def main(page: ft.Page):
     page.title = "Ludo Board (6x6 homes)"
@@ -31,6 +48,9 @@ def main(page: ft.Page):
         if (row, col) == (7, 7):
             color = ft.Colors.GREY_900
 
+        if (row,col)  in home_run:
+            color = ft.Colors.GREEN
+
         # White cross path (vertical and horizontal)
         elif row in [6, 7, 8] or col in [6, 7, 8]:
             color = ft.Colors.WHITE
@@ -56,13 +76,15 @@ def main(page: ft.Page):
             icon = ft.Image(src=os.path.abspath(arrows[(row,col)]),width=30, height=30)
             print(icon)
 
+
         return ft.Container(
             width=40,  # Cell size
             height=40,  # Cell size
             bgcolor=color,
             alignment=ft.alignment.center,
             border=ft.border.all(1, ft.Colors.BLACK),  # Added border
-            content=ft.Column([icon or ft.Text("", size=20,color=ft.Colors.BLUE)]), padding=0, margin=0  # No padding/margin
+            content=ft.Column([icon or ft.Text(f"{row},{col}", size=8,color=ft.Colors.BLUE)]), padding=0, margin=0  # No padding/margin
+
         )
 
     for row in range(15):
@@ -71,6 +93,8 @@ def main(page: ft.Page):
             row_cells.append(get_cell(row, col))
         # Use ft.Row with spacing set to 0
         board.append(ft.Row(row_cells, spacing=0, run_spacing=0,alignment=ft.MainAxisAlignment.CENTER))
+    page.update()
+    # draw_lines_to_center(page)
     page.update()
 #
 #     # Use ft.Column to stack rows without gaps
